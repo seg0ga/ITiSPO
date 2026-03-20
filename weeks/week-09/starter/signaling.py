087f02107089616b00b3841bfd7e354397e7b4c7
Segoga <demin_s22@mail.ru>
2026-03-20 22:49:53 +0700

    Сделана лабораторная работа №9

--- weeks/week-09/starter/signaling.py
+++ weeks/week-09/starter/signaling.py
@@ -1,16 +1,12 @@
 import asyncio
 import websockets
 
-# Простой Signaling сервер для WebRTC
-# Он должен пересылать сообщения от одного клиента всем остальным (или конкретному собеседнику)
-
 CONNECTIONS = set()
 
 async def handler(websocket):
     CONNECTIONS.add(websocket)
     try:
         async for message in websocket:
-            # Рассылаем сообщение всем остальным подключенным клиентам
             for conn in CONNECTIONS:
                 if conn != websocket:
                     await conn.send(message)
@@ -18,8 +14,8 @@ async def handler(websocket):
         CONNECTIONS.remove(websocket)
 
 async def main():
-    async with websockets.serve(handler, "localhost", 8765):
-        await asyncio.Future()  # run forever
+    async with websockets.serve(handler, "localhost", 8181):
+        await asyncio.Future()
 
 if __name__ == "__main__":
     asyncio.run(main())
